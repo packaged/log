1309b5b189dfd71cdf03238f62031dd96fc51b43
Brooke Bryan <brooke@bajb.net>
2018-12-12 11:07:06 +0000

    Static Logger

---
+++ src/Log.php
@@ -0,0 +1,57 @@
+<?php
+namespace Packaged\Log;
+
+use Psr\Log\LoggerInterface;
+
+class Log
+{
+  /**
+   * @var LoggerInterface
+   */
+  private static $_logger;
+
+  public function __construct(LoggerInterface $logger)
+  {
+    self::$_logger = $logger;
+  }
+
+  public static function emergency($message, array $context = [])
+  {
+    self::$_logger->emergency($message, $context);
+  }
+
+  public static function alert($message, array $context = [])
+  {
+    self::$_logger->alert($message, $context);
+  }
+
+  public static function critical($message, array $context = [])
+  {
+    self::$_logger->critical($message, $context);
+  }
+
+  public static function error($message, array $context = [])
+  {
+    self::$_logger->error($message, $context);
+  }
+
+  public static function warning($message, array $context = [])
+  {
+    self::$_logger->warning($message, $context);
+  }
+
+  public static function notice($message, array $context = [])
+  {
+    self::$_logger->notice($message, $context);
+  }
+
+  public static function info($message, array $context = [])
+  {
+    self::$_logger->info($message, $context);
+  }
+
+  public static function debug($message, array $context = [])
+  {
+    self::$_logger->debug($message, $context);
+  }
+}
