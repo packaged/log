9fba7dfd22673bebd012467ab3626f9b27e2f185
Tom Kay <oridan82@gmail.com>
2019-11-08 12:00:34 +0000

    add fallback logger

--- src/Log.php
+++ src/Log.php
@@ -13,46 +13,66 @@ class Log
   public static function bind(LoggerInterface $logger): LoggerInterface
   {
     self::$_logger = $logger;
-    return self::$_logger;
+    return self::_getLogger();
+  }
+
+  public static function unbind(): void
+  {
+    self::$_logger = null;
+  }
+
+  protected static function _getLogger(): LoggerInterface
+  {
+    return self::$_logger ?: static::_fallbackLogger();
+  }
+
+  protected static function _fallbackLogger(): LoggerInterface
+  {
+    static $failoverLogger;
+    if($failoverLogger === null)
+    {
+      $failoverLogger = new ErrorLogLogger();
+    }
+    return $failoverLogger;
   }
 
   public static function emergency($message, array $context = [])
   {
-    self::$_logger->emergency($message, $context);
+    self::_getLogger()->emergency($message, $context);
   }
 
   public static function alert($message, array $context = [])
   {
-    self::$_logger->alert($message, $context);
+    self::_getLogger()->alert($message, $context);
   }
 
   public static function critical($message, array $context = [])
   {
-    self::$_logger->critical($message, $context);
+    self::_getLogger()->critical($message, $context);
   }
 
   public static function error($message, array $context = [])
   {
-    self::$_logger->error($message, $context);
+    self::_getLogger()->error($message, $context);
   }
 
   public static function warning($message, array $context = [])
   {
-    self::$_logger->warning($message, $context);
+    self::_getLogger()->warning($message, $context);
   }
 
   public static function notice($message, array $context = [])
   {
-    self::$_logger->notice($message, $context);
+    self::_getLogger()->notice($message, $context);
   }
 
   public static function info($message, array $context = [])
   {
-    self::$_logger->info($message, $context);
+    self::_getLogger()->info($message, $context);
   }
 
   public static function debug($message, array $context = [])
   {
-    self::$_logger->debug($message, $context);
+    self::_getLogger()->debug($message, $context);
   }
 }
